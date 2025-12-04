14f862d3eafe83532813d94bf53017566b0b4037
Jong Woo Nam <jwnam0709@gmail.com>
2025-12-04 09:46:45 +0900

    version auto update

--- src/fasrat/__init__.py
+++ src/fasrat/__init__.py
@@ -5,10 +5,14 @@ A Python package for computing area-weighted spatial reaggregation weights
 between shapefile geometries and raster pixels.
 """
 
-__version__ = "0.1.0"
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback version for development installations without setuptools-scm
+    __version__ = "0.0.0+unknown"
 
 from .geometry import *
 from .constants import *
 from .process import compute_raster_weights
 
-__all__ = ["compute_raster_weights", "NON_CONTIGUOUS_STATES"]
+__all__ = ["compute_raster_weights", "NON_CONTIGUOUS_STATES", "__version__"]
